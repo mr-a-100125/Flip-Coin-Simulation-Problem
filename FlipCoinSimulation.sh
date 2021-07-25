cc8f4206b0f8a5ff9ff540574b3e8bb676e5d4c2
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 14:39:00 +0530

    Simulating start by flipping a coin to displaying Heads or Tails as winner.

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "Welcome to Flip Coin Simulation Problem."
+
+flipCoin=$((RANDOM%2))
+if [ $flipCoin -eq 0 ]
+then
+	heads=$(($heads+1))
+	echo "heads is winner."
+else
+	tails=$(($tails+1))
+	echo "tails is winner." 
+fi
